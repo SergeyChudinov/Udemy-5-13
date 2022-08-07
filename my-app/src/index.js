9282cfdfeaf7105493a604702a32197784205de7
SergeyChudinov <sergeychudinov1986@gmail.com>
2022-08-07 12:11:21 +0500

    Make lesson-7-02

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import { useState } from 'react';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,20 +10,12 @@ class App extends React.Component {
       counter: this.props.counter
     }
   }
-  incCounter = () => {
-    if (this.state.counter >= 50) {
+  changeConter = (i) => {
+    if (this.state.counter <= -50 || this.state.counter >= 50) {
       return;
     }
     this.setState(state => ({
-      counter: state.counter + 1
-    }))
-  }
-  decCounter = () => {
-      if (this.state.counter <= -50) {
-        return;
-      }
-    this.setState(state => ({
-      counter: state.counter - 1
+      counter: state.counter + i
     }))
   }
   rndCounter = () => {
@@ -42,8 +34,8 @@ class App extends React.Component {
       <div className='app'>
         <div className='counter'>{this.state.counter}</div>
         <div className='controls'>
-          <button onClick={this.incCounter}>INC</button>
-          <button onClick={this.decCounter}>DEC</button>
+          <button onClick={() => this.changeConter(1)}>INC</button>
+          <button onClick={() => this.changeConter(-1)}>DEC</button>
           <button onClick={this.rndCounter}>RND</button>
           <button onClick={this.resetCounter}>RESET</button>
         </div>
@@ -52,6 +44,33 @@ class App extends React.Component {
   }
 }
 
+const App2 = (props) => {
+  const [counter, setConter] = useState(0)
+  const changeConter = (i) => {
+    if (counter <= -50 || counter >= 50) {
+      return
+    }
+    setConter(counter => counter + i)
+  }
+  const rndCounter = () => {
+    let random = Math.floor((Math.random() * 101) - 50);
+    setConter(random)
+  }
+  const resetCounter = () => {
+    setConter(0)
+  }
+  return (
+    <div className='app'>
+      <div className='counter'>{counter}</div>
+      <div className='controls'>
+        <button onClick={() => changeConter(1)}>INC</button>
+        <button onClick={() => changeConter(-1)}>DEC</button>
+        <button onClick={rndCounter}>RND</button>
+        <button onClick={resetCounter}>RESET</button>
+      </div>
+    </div>
+  )
+}
 
 
 // ReactDOM.render(<App counter={0}/>, document.getElementById('app'));
@@ -60,5 +79,6 @@ const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
     <App counter={0}/>
+    <App2/>
   </React.StrictMode>
 );
